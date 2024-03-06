f9e8fa96cb97ce517bc8be186c2e7f123b87827c
Булочка <lerastep22@gmail.com>
2024-03-07 04:13:31 +0700

    upd class person

---
+++ src/entity/Person.java
@@ -0,0 +1,15 @@
+package entity;
+
+public class Person {
+    private int id;
+    private int age;
+    private String name;
+
+    public Person(int age) {
+        this.age = age;
+        this.id = id;
+    }
+    public Person(String name) {
+        this.name = name;
+    }
+}
