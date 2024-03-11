9facae773e26d17ac9abc7d243cf6b176472242c
Булочка <lerastep22@gmail.com>
2024-03-12 05:46:18 +0700

    solving tasks 7.1 and 7.2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import entity.Person;
+
+import java.io.File;
+import java.io.IOException;
+
+import static entity.Person.loadFrom;
+import static entity.Person.saveTo;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        }
+        File file = new File("test.txt");
+        Person f = new Person(23, 1);
+        saveTo(file, f);
+        loadFrom(file);
     }
+}
+
--- src/entity/Person.java
+++ src/entity/Person.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.io.*;
+
 public class Person {
     private int id;
     private int age;
@@ -9,8 +11,46 @@ public class Person {
         this.age = age;
         this.id = id;
     }
+
     public Person(String name, int id) {
         this.name = name;
         this.id = id;
     }
+
+    public static void saveTo(File file, Person person) {
+        try (FileOutputStream outputStream = new FileOutputStream(file)) {
+            try (PrintWriter writer = new PrintWriter(outputStream)) {
+                writer.write(person.id);
+                writer.write(person.age);
+                writer.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Не удалось открыть файл" + e.getMessage());
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("Ошибка записи в файл" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void loadFrom(File file) {
+        try (FileInputStream stream = new FileInputStream(file)) {
+            try {
+                int i;
+                while ((i = stream.read()) != 1) {
+                    System.out.println((char) i);
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Не удалось открыть файл" + e.getMessage());
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файла" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
 }
+
+
