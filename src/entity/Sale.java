b08e98e35c2a9c0a5633f3397c018aa3ba2d0d09
Булочка <lerastep22@gmail.com>
2024-04-05 23:42:47 +0700

    10.2

---
+++ src/entity/Sale.java
@@ -0,0 +1,39 @@
+package entity;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+//10.2
+public class Sale implements Iterable<Product>{
+    private final int id;
+    double amount;
+    Person person;
+    Map<Product, Double> products = new TreeMap<>();
+
+    public Sale(int id) {
+        this.id = id;
+    }
+
+    public Sale(Map<Product, Double> products, int id) {
+        this.id = id;
+        this.products = new TreeMap<>();
+    }
+
+    @Override
+    public Iterator<Product> iterator() {
+        return products.keySet().iterator();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+}
